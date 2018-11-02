a56797167dc0cf80fb1c0e741c2702e9c6356b39
propersam <a.samuel@suretrade.ng>
2018-11-02 23:46:50 +0100

    remove refresh function that damages the script

--- getHostel.py
+++ getHostel.py
@@ -39,11 +39,11 @@ def get_num_of_hostel_available(browser):
 	return len(hostel_list)
 
 
-def start_hostel_application(browser, num_of_hostel, count_refresh):
+def start_hostel_application(browser, num_of_hostel):
 
 	tries = 10 #1000
 
-	seconds = 10
+	seconds = 15
 	max_num = num_of_hostel - 1
 
 	count = 1
@@ -55,12 +55,13 @@ def start_hostel_application(browser, num_of_hostel, count_refresh):
 
 	
 
-		# refresh page at every 10 counts before continuing
-		if count in count_refresh:
-			browser.refresh()
+		# # refresh page at every 10 counts before continuing
+		# if count in count_refresh:
+		# 	browser.refresh()
 	
 		# Get any of the random hostel buttons
-		num = random.randint(0,max_num)
+		num = 0
+		num += random.randint(0,max_num)
 
 		hostel_btn = 'ContentPlaceHolder1_DataList1_btnOption_'+str(num)
 		hostel = browser.find_element_by_id(hostel_btn)
@@ -73,21 +74,26 @@ def start_hostel_application(browser, num_of_hostel, count_refresh):
 			
 			# check if there is an alert of no hostel available
 			time.sleep(8)
-			Alert = browser.switch_to.alert
+			if browser.switch_to.alert:
+				Alert = browser.switch_to.alert
+				print(Alert.text)
+				time.sleep(2)
+				Alert.accept()
+			else:
+				count += 1
+				continue
+
 			print(f"done...")
 
-			if Alert:
-				print(Alert.text)
-				time.sleep(2)		
-			Alert.accept()
-
+					
+			
+			count += 1
 			# put in waiting time here
 			print(f"\nwaiting for {seconds} seconds before retrying another hostel...")
 			time.sleep(seconds)
 			print(f'ready...\ndone trials: {count}/{tries}')
 			
-			count += 1
-
+			
 			# # try again on another hostel after waiting
 			# return start_hostel_application(browser, num_of_hostel, count_refresh)
 
@@ -146,12 +152,13 @@ max_hostel_available = get_num_of_hostel_available(browser_driver)
 print(f"There are only {max_hostel_available} hostels available.")
 
 
-# first get multiples of 5 for purpose of refreshing
-mult_of_five = [x*5 for x in range(1,201)] 
+# # first get multiples of 5 for purpose of refreshing
+# mult_of_five = [x*5 for x in range(1,201)] 
+
 print()
 print('='*5, '-| Starting hostel Application Process now |-', '='*5)
 # start applying for any random hostel
-start_hostel_application(browser_driver, max_hostel_available, mult_of_five)
+start_hostel_application(browser_driver, max_hostel_available)
 
 # print('When you are done and you are yet to get hostel click on Get hostel to continue the hostel application...')
 
@@ -159,9 +166,12 @@ start_hostel_application(browser_driver, max_hostel_available, mult_of_five)
 
 print('The program has stopped temporarily..')
 print("To continue trying press '[Y]' or any other key to quit")
-reply = input().lower()
-if reply == 'y':
-	start_hostel_application(browser_driver, max_hostel_available, mult_of_five)
-else:
+try:
+	reply = input().lower()
+	if reply == 'y':
+		start_hostel_application(browser_driver, max_hostel_available)
+except:
+	print('error', sys.exc_info()[0])
+finally:
 	terminate_program(browser_driver)
 
