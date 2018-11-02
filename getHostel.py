8afa0b0ee5d69d216dcb42b16f6e52065a2f0173
propersam <a.samuel@suretrade.ng>
2018-11-02 22:57:25 +0100

    chang recursion to while loop and fix count

--- getHostel.py
+++ getHostel.py
@@ -46,55 +46,57 @@ def start_hostel_application(browser, num_of_hostel, count_refresh):
 	seconds = 10
 	max_num = num_of_hostel - 1
 
-	global count
+	count = 1
 
-	if count >= tries:
-		print('maximum tries reached..')
-		return
+	while count <= tries:
+		# print('maximum tries reached..')
+		# return
 
 	
 
-	# refresh page at every 10 counts before continuing
-	if count in count_refresh:
-		browser.refresh()
+		# refresh page at every 10 counts before continuing
+		if count in count_refresh:
+			browser.refresh()
 	
-	# Get any of the random hostel buttons
-	num = random.randint(0,max_num)
+		# Get any of the random hostel buttons
+		num = random.randint(0,max_num)
 
-	hostel_btn = 'ContentPlaceHolder1_DataList1_btnOption_'+str(num)
-	hostel = browser.find_element_by_id(hostel_btn)
-	print(f"\n Trying {hostel.get_attribute('value')} ... ")
-	time.sleep(3.5)
-	hostel.click() # click button
-	
-
-	try:
+		hostel_btn = 'ContentPlaceHolder1_DataList1_btnOption_'+str(num)
+		hostel = browser.find_element_by_id(hostel_btn)
+		print(f"\n Trying {hostel.get_attribute('value')} ... ")
+		time.sleep(3.5)
+		hostel.click() # click button
 		
-		# check if there is an alert of no hostel available
-		time.sleep(8)
-		Alert = browser.switch_to.alert
-		print(f"done...")
 
-		if Alert:
-			print(Alert.text)
-			time.sleep(2)		
-		Alert.accept()
+		try:
+			
+			# check if there is an alert of no hostel available
+			time.sleep(8)
+			Alert = browser.switch_to.alert
+			print(f"done...")
 
-		# put in waiting time here
-		print(f"\nwaiting for {seconds} seconds before retrying another hostel...")
-		time.sleep(seconds)
-		print(f'ready...\ndone trials: {count}/{tries}')
-		count += 1
+			if Alert:
+				print(Alert.text)
+				time.sleep(2)		
+			Alert.accept()
 
-		# try again on another hostel after waiting
-		return start_hostel_application(browser, num_of_hostel, count_refresh)
+			# put in waiting time here
+			print(f"\nwaiting for {seconds} seconds before retrying another hostel...")
+			time.sleep(seconds)
+			print(f'ready...\ndone trials: {count}/{tries}')
+			
+			count += 1
 
-	except:
-		print("I'm Confused: This current page is strange and I need your attention..")
-		print("It's either there is an error from server or this is a new page I don't recognise..\n")		
-		# There should be a form of notification to get attention here
-		time.sleep(5) # Let the user see something
-		return
+			# # try again on another hostel after waiting
+			# return start_hostel_application(browser, num_of_hostel, count_refresh)
+
+		except:
+			print('Error:', sys.exc_info()[0])
+			print("I'm Confused: This current page is strange and I need your attention..")
+			print("It's either there is an error from server or this is a new page I don't recognise..\n")		
+			# There should be a form of notification to get attention here
+			time.sleep(5) # Let the user see something
+			break
 
 def terminate_program(browser):
 	if browser:
@@ -144,11 +146,11 @@ print(f"There are only {max_hostel_available} hostels available.")
 
 
 # first get multiples of 5 for purpose of refreshing
-mult_of_ten = [x*10 for x in range(1,101)] 
+mult_of_five = [x*5 for x in range(1,201)] 
 print()
 print('='*5, '-| Starting hostel Application Process now |-', '='*5)
 # start applying for any random hostel
-start_hostel_application(browser_driver, max_hostel_available, mult_of_ten)
+start_hostel_application(browser_driver, max_hostel_available, mult_of_five)
 
 # print('When you are done and you are yet to get hostel click on Get hostel to continue the hostel application...')
 
@@ -158,7 +160,7 @@ print('The program has stopped temporarily..')
 print("To continue trying press '[Y]' or any other key to quit")
 reply = input().lower()
 if reply == 'y':
-	start_hostel_application(browser_driver, max_hostel_available, mult_of_ten)
+	start_hostel_application(browser_driver, max_hostel_available, mult_of_five)
 else:
 	terminate_program(browser_driver)
 
